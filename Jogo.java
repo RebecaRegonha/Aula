0f02eee21ffd06cfcbb08ac7588c17d1f6338ce3
Rebeca Regonha <rebecaregonha@hotmail.com>
2024-04-05 20:30:34 -0300

    aula

---
+++ Jogo.java
@@ -0,0 +1,12 @@
+public class Jogo {
+    public static void main(String[] args) {
+        Personagem cacador = new Personagem ("John",10,0,0); 
+        while (true){
+            cacador.cacar();
+            cacador.comer();
+            cacador.dormir();
+            cacador.
+        
+        }
+    }
+}
